3c1def901c08eefca1ae8df0fad754aee1bfa2db
Luizhnrs <Luizhnrsg@gmail.com>
2024-05-13 10:36:27 -0300

    Api Host Dll update

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+776377e6308d41a370dc8d359c7dd765c882131b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3ef159390e0041e7831a107c77c59717ddce70d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
