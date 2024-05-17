865ad570775a85a539171370c612a1f547328c99
Luizhnrs <Luizhnrsg@gmail.com>
2024-05-17 12:01:21 -0300

    Feat: Create Interface implemented in Stock Repository

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.Dtos.Stock;
 using api.Interfaces;
 using api.Migrations;
 using api.Models;
@@ -17,9 +18,32 @@ namespace api.Repository
         {
             _context = context;
         }
+
+        public async Task<Stock?> CreateAsync(Stock stockModel)
+        {
+            await _context.Stock.AddAsync(stockModel);
+            await _context.SaveChangesAsync();
+            return stockModel;
+        }
+
+        public Task<Stock?> DeleteByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
         public Task<List<Stock>> GetAllAsync()
         {
             return _context.Stock.ToListAsync();
+        }
+
+        public Task<Stock?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto)
+        {
+            throw new NotImplementedException();
         }
     }
 }  
